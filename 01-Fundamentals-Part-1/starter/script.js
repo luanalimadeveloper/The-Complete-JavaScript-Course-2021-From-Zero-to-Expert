728d51907288b37bb237a75565ffa697210e6500
luanalimadeveloper <luanalima.developer@gmail.com>
2021-07-24 16:31:40 -0300

    Values and Variables

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,5 +1,23 @@
 let js = "amazing";
-if (js == "amazing") alert("Javascript is FUN");
-
-40 + 8 + 23 - 10;
 console.log(40 + 8 + 23 - 10);
+
+console.log("Luana");
+console.log(23);
+
+let firstName = "Fredy";
+
+console.log(firstName);
+
+let jonas_matilda = 'JM';
+let $function = 27;
+
+let person = 'luana';
+let PI = 3.1415;
+
+let myFirstJob = 'Coder';
+let myCurrentJob = 'Teacher';
+
+let job1 = 'programmer';
+let job2 = 'teacher';
+
+console.log(myFirstJob);
