3e60b876c0255541633dd1571f22007298e2084a
luanalimadeveloper <luanalima.developer@gmail.com>
2021-07-26 16:28:41 -0300

    Operator Precedence

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -69,6 +69,7 @@ console.log(lastName);
 */
 
 
+/*
 //BASIC OPERATORS
 // MAth operators 
 const now = 2037;
@@ -97,3 +98,17 @@ console.log(ageSarah >= 18);
 
 const isFullAge = ageSarah >= 18;
 console.log(now -1991 > now - 2018);
+*/
+
+const now = 2037;
+const ageJonas = now - 1991;
+const ageSarah = now - 2018; 
+
+console.log(now -1991 > now - 2018);
+
+let x,y;
+x = y = 25 - 10 - 5; // x = y = 10, x = 10
+console.log(x, y);
+
+const averageAge = (ageJonas + ageSarah) / 2
+console.log(ageJonas, ageSarah, averageAge);
