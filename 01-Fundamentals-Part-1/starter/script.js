5a5f7d2973b4c3ced0d53f5ffcd5741fdb398db7
luanalimadeveloper <luanalima.developer@gmail.com>
2021-07-27 20:56:36 -0300

    Taking Decisions: if / else Statements

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -150,6 +150,7 @@ console.log(BMIMark, BMIJohn, markHigherBMI);
 ////////////////////////////////////
 //Strings and Template Literals
 
+/*
 const firstName = 'Jonas';
 const job = 'teacher';
 const birthYear = 1991;
@@ -170,3 +171,30 @@ lines');
 console.log(`String
 multiple
 lines`);
+*/
+
+
+////////////////////////////////////
+// Taking Decisions: if / else Statements
+const age = 15;
+
+if (age >= 18) {
+  console.log('Sarah can start driving license 🚗');
+} else {
+  const yearsLeft = 18 - age;
+  console.log(`Sarah is too young. Wait another ${yearsLeft} years :)`);
+}
+
+const birthYear = 2012;
+let century;
+
+if (birthYear <= 2000) {
+  century = 20;
+} else {
+  century = 21;
+}
+
+console.log(century);
+
+
+////////////////////////////////////
