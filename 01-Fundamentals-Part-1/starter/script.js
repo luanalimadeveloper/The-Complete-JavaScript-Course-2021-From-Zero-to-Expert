560dd5bfbf1217c4e4f6a4257933583a9ee7102d
luanalimadeveloper <luanalima.developer@gmail.com>
2021-07-28 15:49:26 -0300

    Truthy and Falsy Values

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -234,7 +234,7 @@ if (BMIMark > BMIJohn) {
 ////////////////////////////////////
 // Type Conversion and Coercion
 // type conversion
-
+/*
 const inputYear = '1991';
 console.log(Number(inputYear), inputYear);
 console.log(Number(inputYear) + 18);
@@ -252,6 +252,31 @@ console.log('23' > '18');
 
 let n = '1' + 1; // '11'
 n = n - 1;
+*/
+
+////////////////////////////////////
+// Truthy and Falsy Values
+// 5 falsy values: 0, '', undefined, null, NaN
+
+console.log(Boolean(0));
+console.log(Boolean(undefined));
+console.log(Boolean('Jonas'));
+console.log(Boolean({}));
+console.log(Boolean(''));
+
+const money = 100;
+if (money) {
+  console.log("Don't spend it all ;)");
+} else {
+  console.log('You should get a job!');
+}
+
+let height = 0;
+if (height) {
+  console.log('YAY! Height is defined');
+} else {
+  console.log('Height is UNDEFINED');
+}
 
 
 ////////////////////////////////////
