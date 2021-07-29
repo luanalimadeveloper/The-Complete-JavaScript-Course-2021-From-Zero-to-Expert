4542f1fba22416e53de5f726c804fc77cdbbc7ff
luanalimadeveloper <luanalima.developer@gmail.com>
2021-07-29 14:47:52 -0300

    Statements and Expressions

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -382,6 +382,8 @@ if (scoreDolphins > scoreKoalas && scoreDolphins >= 100) {
 
 ////////////////////////////////////
 // The switch Statement
+
+/*
 const day = 'friday';
 
 switch (day) {
@@ -421,4 +423,17 @@ if (day === 'monday') {
 } else {
   console.log('Not a valid day!');
 }
+*/
+
+
+////////////////////////////////////
+// Statements and Expressions
+3 + 4
+1991
+true && false && !false
+if (23 > 10) {
+  const str = '23 is bigger';
+}
+const me = 'Jonas';
+console.log(`I'm ${2037 - 1991} years old ${me}`);
 ////////////////////////////////////
