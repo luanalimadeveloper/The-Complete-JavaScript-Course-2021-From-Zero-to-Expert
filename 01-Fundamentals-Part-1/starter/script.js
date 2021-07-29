914879be12ab84adee7ebfc097b1c649305e874c
luanalimadeveloper <luanalima.developer@gmail.com>
2021-07-29 18:50:55 -0300

    The Conditional (Ternary) Operator

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -428,6 +428,7 @@ if (day === 'monday') {
 
 ////////////////////////////////////
 // Statements and Expressions
+/*
 3 + 4
 1991
 true && false && !false
@@ -436,4 +437,20 @@ if (23 > 10) {
 }
 const me = 'Jonas';
 console.log(`I'm ${2037 - 1991} years old ${me}`);
+*/
 ////////////////////////////////////
+// The Conditional (Ternary) Operator
+const age = 23;
+// age >= 18 ? console.log('I like to drink wine 🍷') : console.log('I like to drink water 💧');
+
+const drink = age >= 18 ? 'wine 🍷' : 'water 💧';
+console.log(drink);
+
+let drink2;
+if (age >= 18) {
+  drink2 = 'wine 🍷';
+} else {
+  drink2 = 'water 💧';
+}
+console.log(drink2);
+console.log(`I like to drink ${age >= 18 ? 'wine 🍷' : 'water 💧'}`);
