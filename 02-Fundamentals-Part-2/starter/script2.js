4cae0e8f0694e0637c8c570a17be1428ab7866c0
luanalimadeveloper <luanalima.developer@gmail.com>
2021-07-30 14:31:15 -0300

    Fundamentals 2: Functions

--- 02-Fundamentals-Part-2/starter/script2.js
+++ 02-Fundamentals-Part-2/starter/script2.js
@@ -1,10 +1,40 @@
 'use strict';
 
+
+///////////////////////////////////////
+// Activating Strict Mode
+/*
 let hasDriversLicense = false;
 const passTest = true;
 
 if (passTest) hasDriversLicense = true;
-if (hasDriversLicense) console.log('I can drive');
+if (hasDriversLicense) console.log('I can drive :D');
 
-//const interface = 'Audio';
-//const private = 543;
+// const interface = 'Audio';
+// const private = 534;
+*/
+
+///////////////////////////////////////
+// Functions
+
+function logger() {
+    console.log('My name is Luana');
+}
+
+// calling / running / invoking function
+logger();
+logger();
+logger();
+
+function fruitProcessor(apples, oranges) {
+    const juice = `Juice with ${apples} apples and ${oranges} oranges.`;
+    return juice;
+}
+const appleJuice = fruitProcessor(5, 0);
+console.log(appleJuice);
+const appleOrangeJuice = fruitProcessor(2, 4);
+console.log(appleOrangeJuice);
+const num = Number('23');
+
+
+///////////////////////////////////////
