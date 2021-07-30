125af832a283f7be4bbff8222ab4eab3b09ee8e8
luanalimadeveloper <luanalima.developer@gmail.com>
2021-07-30 16:01:38 -0300

    Function Declaration and Function Expressions

--- 02-Fundamentals-Part-2/starter/script2.js
+++ 02-Fundamentals-Part-2/starter/script2.js
@@ -16,7 +16,7 @@ if (hasDriversLicense) console.log('I can drive :D');
 
 ///////////////////////////////////////
 // Functions
-
+/*
 function logger() {
     console.log('My name is Luana');
 }
@@ -35,6 +35,22 @@ console.log(appleJuice);
 const appleOrangeJuice = fruitProcessor(2, 4);
 console.log(appleOrangeJuice);
 const num = Number('23');
-
+*/
 
 ///////////////////////////////////////
+// Function Declarations vs. Expressions
+
+// Function declaration
+function calcAge1(birthYear) {
+    return 2037 - birthYear; 
+}
+const age1 =  calcAge1(1991);
+
+// Function expression
+const calcAge2 = function(birthYear) {
+    return 2037 - birthYear;
+}
+
+const age2 = calcAge2(1991);
+
+console.log(age1,age2);
