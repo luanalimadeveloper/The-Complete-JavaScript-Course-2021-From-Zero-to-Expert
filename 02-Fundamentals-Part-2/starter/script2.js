33dab4d167408a3ba2c061c3526dff3e8d9e2311
luanalimadeveloper <luanalima.developer@gmail.com>
2021-07-30 17:05:18 -0300

    Arrow functions

--- 02-Fundamentals-Part-2/starter/script2.js
+++ 02-Fundamentals-Part-2/starter/script2.js
@@ -40,6 +40,7 @@ const num = Number('23');
 ///////////////////////////////////////
 // Function Declarations vs. Expressions
 
+/*
 // Function declaration
 function calcAge1(birthYear) {
     return 2037 - birthYear; 
@@ -54,3 +55,21 @@ const calcAge2 = function(birthYear) {
 const age2 = calcAge2(1991);
 
 console.log(age1,age2);
+*/
+
+
+///////////////////////////////////////
+// Arrow functions
+
+const calcAge3 = birthYear => 2037 - birthYear;
+const age3 = calcAge3(1991);
+console.log(age3);
+
+const yearsUntilRetirement = (birthYear, firstName) => {
+    const age = 2037 - birthYear;
+    const retirement = 65 - age;
+    // return retirement; 
+    return  `${firstName} retires in ${retirement} years`;
+}
+
+console.log(yearsUntilRetirement(1991, 'Luana')); console.log(yearsUntilRetirement(1980,'Bob'));
