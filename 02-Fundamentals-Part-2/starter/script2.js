6b1b3ed303f4eac598897aeb539230ee85a37154
luanalimadeveloper <luanalima.developer@gmail.com>
2021-07-31 19:20:34 -0300

    Reviewing Functions

--- 02-Fundamentals-Part-2/starter/script2.js
+++ 02-Fundamentals-Part-2/starter/script2.js
@@ -61,6 +61,7 @@ console.log(age1,age2);
 ///////////////////////////////////////
 // Arrow functions
 
+/*
 const calcAge3 = birthYear => 2037 - birthYear;
 const age3 = calcAge3(1991);
 console.log(age3);
@@ -73,3 +74,33 @@ const yearsUntilRetirement = (birthYear, firstName) => {
 }
 
 console.log(yearsUntilRetirement(1991, 'Luana')); console.log(yearsUntilRetirement(1980,'Bob'));
+*/
+
+
+///////////////////////////////////////
+// Reviewing Functions
+const calcAge = function (birthYeah) {
+    return 2037 - birthYeah;
+}
+
+const yearsUntilRetirement = function (birthYeah, firstName) {
+    const age = calcAge(birthYeah);
+    const retirement = 65 - age;
+
+    if (retirement > 0) {
+        console.log(`${firstName} retires in ${retirement} years`);
+        return retirement;
+    } else {
+        console.log(`${firstName} has already retired 🎉`);
+        return -1;
+    }
+}
+
+
+console.log(yearsUntilRetirement(1991, 'Jonas'));
+console.log(yearsUntilRetirement(1950, 'Mike'));
+
+
+///////////////////////////////////////
+
+
