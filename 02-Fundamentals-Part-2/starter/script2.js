654aad577950a5008af4cace4825f144cb3a4ef3
luanalimadeveloper <luanalima.developer@gmail.com>
2021-08-06 14:04:42 -0300

    Introduction to Objects

--- 02-Fundamentals-Part-2/starter/script2.js
+++ 02-Fundamentals-Part-2/starter/script2.js
@@ -234,6 +234,7 @@ if (friends.includes('Steven')) {
 }
 */
 
+
 ///////////////////////////////////////
 // Coding Challenge #2
 
@@ -248,6 +249,7 @@ HINT: Remember that an array needs a value in each position, and that value can 
 GOOD LUCK 😀
 */
 
+/*
 const calcTip = function(bill) {
   return bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2; 
 }
@@ -259,5 +261,26 @@ const tips = [calcTip(bills[0]), calcTip(bills[1]), calcTip(bills[2])];
 const totals = [bills[0] + tips[0], bills[1] + tips[1], bills[2] + tips[2]];
 
 console.log(bills, tips, totals);
+*/
+
+///////////////////////////////////////
+// Introduction to Objects
+
+const jonasArray = [
+  'Jonas',
+  'Schmedtmann',
+  2037 - 1991,
+  'teacher',
+  ['Michal', 'Peter', 'Steven']
+];
+
+const jonas = {
+  firstname: 'Jonas',
+  lastname: 'Schmedtmann',
+  age: 2037 - 1991,
+  job: 'teacher',
+  friends: ['Michal', 'Peter', 'Steven']
+}; 
+
 
 
