4c1c8ee6e3ca20ad83f4a904f03cc7ee9e800757
luanalimadeveloper <luanalima.developer@gmail.com>
2021-08-21 16:41:57 -0300

    Objects vs. primitives

--- 08-Behind-the-Scenes/script.js
+++ 08-Behind-the-Scenes/script.js
@@ -131,6 +131,7 @@ f();
 // Regular Functions vs. Arrow Functions
 // var firstName = 'Matilda';
 
+/*
 const jonas = {
   firstName: 'Jonas',
   year: 1991,
@@ -174,3 +175,21 @@ var addArrow = (a, b) => {
   return a + b;
 };
 addArrow(2, 5, 8);
+*/
+
+///////////////////////////////////////
+// Objects vs. primitives
+let age = 30;
+let oldAge = age;
+age = 31;
+console.log(age);
+console.log(oldAge);
+
+const me = {
+  name: 'Jonas',
+  age: 30,
+};
+const friend = me;
+friend.age = 27;
+console.log('Friend:', friend);
+console.log('Me', me);
