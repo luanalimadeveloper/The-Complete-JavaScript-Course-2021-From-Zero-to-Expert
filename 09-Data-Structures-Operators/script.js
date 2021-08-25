51f56b2f4e42ea8aa59f918143bdc9158b5cf7b6
luanalimadeveloper <luanalima.developer@gmail.com>
2021-08-25 14:39:14 -0300

    Nullish Coalescing Operator

--- 09-Data-Structures-Operators/script.js
+++ 09-Data-Structures-Operators/script.js
@@ -49,7 +49,20 @@ const restaurant = {
     console.log(otherIngredients);
   },
 };
+////////////////////////////////////
+// Nullish Coalescing Operator
 
+restaurant.numGuests = 0;
+const guests = restaurant.numGuests || 10;
+console.log(guests);
+
+// Nullish: null and undefined (not 0 or '')
+const guestCorrect = restaurant.numGuests ?? 10;
+console.log(guestCorrect);
+
+/*
+////////////////////////////////////
+// Short Circuiting (&& and ||)
 // Or operator
 // The OR operator will return the first truthy value
 console.log(3 || 'Jonas');
