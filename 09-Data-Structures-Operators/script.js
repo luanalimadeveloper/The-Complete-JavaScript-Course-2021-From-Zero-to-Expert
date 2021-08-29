0a9772a5dd11b81640ecf60c56cb9994bfaa8be8
luanalimadeveloper <luanalima.developer@gmail.com>
2021-08-28 22:29:10 -0300

    Optional Chaining

--- 09-Data-Structures-Operators/script.js
+++ 09-Data-Structures-Operators/script.js
@@ -50,6 +50,44 @@ const restaurant = {
 };
 
 ///////////////////////////////////////
+// Optional Chaining
+
+// Optional Chaining - In case we don't get a result from the object or from the array that's here on the left hand side.
+
+if (restaurant.openingHours && restaurant.openingHours.mon)
+  console.log(restaurant.openingHours.mon.open);
+
+// console.log(restaurant.openingHours.mon.open);
+
+// WITH optional chaining
+console.log(restaurant.openingHours.mon?.open);
+console.log(restaurant.openingHours?.mon?.open);
+
+// Example
+const days = ['mon', 'tue', 'wed', 'thu', 'fri', 'sat', 'sun'];
+
+// ?? - Nullish coalescing operator
+//  ? - Optional chaining operator
+for (const day of days) {
+  //console.log(day);
+  const open = restaurant.openingHours[day]?.open ?? 'closed';
+  console.log(`On ${day}, we open at ${open} `);
+}
+
+// Methods
+console.log(restaurant.order?.(0, 1) ?? 'Method does not exist');
+console.log(restaurant.orderRisotto?.(0, 1) ?? 'Method does not exist');
+
+// Arrays
+const users = [{ name: 'Luana', email: 'hello@luana.com' }];
+//const users = [];
+
+console.log(users[0]?.name ?? 'User array empty');
+
+//if (users.length > 0) console.log(users[0].name);
+//else console.log('user array empty');
+
+///////////////////////////////////////
 // Coding Challenge #1
 
 /* 
