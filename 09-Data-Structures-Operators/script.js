7d1834214c0474f198039645d0f30fadfd97b990
luanalimadeveloper <luanalima.developer@gmail.com>
2021-09-04 15:08:31 -0300

    orking with Strings - Part 3

--- 09-Data-Structures-Operators/script.js
+++ 09-Data-Structures-Operators/script.js
@@ -18,6 +18,59 @@ const openingHours = {
 */
 
 ///////////////////////////////////////
+// Working with Strings - Part 3
+
+console.log('a+very+nice+string'.split('+'));
+console.log('Jonas Schmedtmann'.split(' '));
+
+const [firstName, lastname] = 'Jonas Schmedtman'.split(' ');
+
+const newName = ['Mr', firstName, lastname.toUpperCase()].join(' ');
+console.log(newName);
+
+const capitalizeName = function (name) {
+  const names = name.split(' ');
+  const namesUpper = [];
+
+  for (const n of names) {
+    //namesUpper.push(n[0].toUpperCase() + n.slice(1));
+    namesUpper.push(n.replace(n[0], n[0].toUpperCase()));
+  }
+  console.log(namesUpper.join(' '));
+};
+
+capitalizeName('jessica ann smith davis');
+capitalizeName('luana daiana lima');
+
+// Padding
+const message = 'Go to gate 23!';
+console.log(message.padStart(20, '+').padEnd(30, '+'));
+console.log('Luana'.padStart(20, '*').padEnd(30, '*'));
+
+const maskCreditCArd = function (number) {
+  const str = number + ''; // convert to string
+  const last = str.slice(-4);
+  return last.padStart(str.length, '*');
+};
+
+console.log(maskCreditCArd(12345678910));
+console.log(maskCreditCArd(4736276438733));
+console.log(maskCreditCArd(356573));
+
+// Repeat
+
+const message2 = 'Bad waether.. All Departs Delayed...';
+console.log(message2.repeat(5));
+
+const planesInLine = function (n) {
+  console.log(`There are ${n} planes in line ${'✈'.repeat(n)}`);
+};
+
+planesInLine(5);
+planesInLine(3);
+planesInLine(12);
+/*
+///////////////////////////////////////
 // Working with Strings - Part 2
 
 const airline = 'TAP Air Portugal';
