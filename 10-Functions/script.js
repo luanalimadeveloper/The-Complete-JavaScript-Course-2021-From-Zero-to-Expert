3cdaaedd16ce94ac2adce257268fe09f06994a9f
luanalimadeveloper <luanalima.developer@gmail.com>
2021-09-06 15:11:54 -0300

    How Passing Arguments Works: Value vs. Reference

--- 10-Functions/script.js
+++ 10-Functions/script.js
@@ -1,5 +1,44 @@
 'use strict';
 
+///////////////////////////////////////
+// How Passing Arguments Works: Value vs. Reference
+
+const flight = 'LH234';
+const luana = {
+  name: 'Luana Lima',
+  passaport: 241218,
+};
+
+const checkIn = function (flightNum, passenger) {
+  flightNum = 'LH999';
+  passenger.name = 'Mrs. ' + passenger.name;
+
+  if (passenger.passaport === 241218) {
+    alert('Checked in');
+  } else {
+    alert('Wrong passport!');
+  }
+};
+
+// checkIn(flight, luana);
+// console.log(flight);
+// console.log(luana);
+
+// Is the same as doing
+//const flightNum =flight;
+//const passenger = luana;
+
+const newPassport = function (penson) {
+  person.passaport = Math.trunc(Math.random() * 1000000);
+};
+
+newPassport(luana);
+checkIn(flight, luana);
+
+///////////////////////////////////////
+// Default Parameters
+/*
+
 const bookings = [];
 
 const createBooking = function (
@@ -25,3 +64,4 @@ createBooking('LH123', 2, 800);
 createBooking('LH123', 2);
 createBooking('LH123', 5);
 createBooking('LH123', undefined, 1000);
+*/
