2ba66dbf0ffa9042d280154b7e66c737c675a5d4
luanalimadeveloper <luanalima.developer@gmail.com>
2021-09-07 00:01:45 -0300

    he call and apply Methods

--- 10-Functions/script.js
+++ 10-Functions/script.js
@@ -1,6 +1,68 @@
 'use strict';
 
 ///////////////////////////////////////
+// The call and apply Methods
+
+const lufthansa = {
+  airline: 'Lufthansa',
+  iataCode: 'LH',
+  bookings: [],
+  // book: funtion() {}
+  book(flightNum, name) {
+    console.log(
+      `${name} booked a seat on ${this.airline} flight ${this.iataCode} ${flightNum}`
+    );
+    this.bookings.push({
+      flight: `${this.iataCode} ${flightNum}`,
+      name,
+    });
+  },
+};
+
+lufthansa.book(239, 'Jonas Schmedtmann');
+lufthansa.book(635, 'Luana Lima');
+console.log(lufthansa);
+
+const eurowings = {
+  airline: 'Eurowings',
+  iataCode: 'EW',
+  bookings: [],
+};
+
+const book = lufthansa.book;
+
+// Does not work
+//book(23, 'Sarah Willians');
+
+// Remenber :
+// A function is really just an object, and objects have methods, and therefore, functions can have methods too.
+
+// Call Method
+book.call(eurowings, 23, 'Sarah Williams');
+console.log(eurowings);
+
+book.call(lufthansa, 239, 'Mary Silva');
+console.log(lufthansa);
+
+const swiss = {
+  airline: 'Swiss Air Lines',
+  iataCode: 'LX',
+  bookings: [],
+};
+
+book.call(swiss, 56, 'John Coper');
+console.log(swiss);
+
+// Apply method
+const flightData = [583, 'Georde Cooper'];
+book.apply(swiss, flightData);
+console.log(swiss);
+
+// Modern JavaScript
+book.call(swiss, ...flightData);
+
+/*
+///////////////////////////////////////
 // Functions Returning Functions
 
 const greet = function (greeting) {
