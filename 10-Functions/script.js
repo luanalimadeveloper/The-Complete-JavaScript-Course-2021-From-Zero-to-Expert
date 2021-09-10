20125dc3f84e52918a66cffe917b7a8f755f0d1b
luanalimadeveloper <luanalima.developer@gmail.com>
2021-09-10 17:36:51 -0300

    More Closure Examples

--- 10-Functions/script.js
+++ 10-Functions/script.js
@@ -1,9 +1,57 @@
 'use strict';
 
 ///////////////////////////////////////
+// More Closure Examples
+
+// Example 1
+let f;
+
+const g = function () {
+  const a = 23;
+  f = function () {
+    console.log(a * 2);
+  };
+};
+
+const h = function () {
+  const b = 777;
+  f = function () {
+    console.log(b * 2);
+  };
+};
+
+g();
+f();
+console.dir(f);
+// Re-assigning f fynction
+h();
+f();
+console.dir(f);
+
+// Example 2
+
+const boardPassengers = function (n, wait) {
+  const perGroup = n / 3;
+
+  setTimeout(function () {
+    console.log(`We are now boarding all ${n} passengers`);
+    console.log(`There are 3 groups, each with ${perGroup} passengers`);
+  }, wait * 1000);
+
+  console.log(`Will start boarding in ${wait} seconds`);
+};
+
+const perGroup = 1000;
+boardPassengers(180, 3);
+
+// setTimeout(function () {
+//   console.log('TIMER');
+// }, 1000);
+
+///////////////////////////////////////
 // Closures
 // A closure makes a function remember all the variables that existed at the function's birthplace
-
+/*
 const secureBooking = function () {
   let passengerCount = 0;
 
@@ -20,11 +68,11 @@ booker();
 booker();
 
 console.dir(booker);
+*/
 
-/*
 ///////////////////////////////////////
 // Immediately Invoked Function Expressions (IIFE)
-
+/*
 const runOnce = function () {
   console.log('This never run again');
 };
