4838d1112323b6a85e1b7047cc57805eb770ba61
luanalimadeveloper <luanalima.developer@gmail.com>
2021-09-11 14:14:38 -0300

    Coding Challenge #2

--- 10-Functions/script.js
+++ 10-Functions/script.js
@@ -1,7 +1,30 @@
 'use strict';
+///////////////////////////////////////
+// Coding Challenge #2
+
+/*
+1. Take the IIFE below and at the end of the function, attach an event listener that 
+changes the color of the selected h1 element ('header') to blue, each time 
+the body element is clicked. Do not select the h1 element again
+
+2. And now explain to yourself (or someone around you) why this worked! Take all 
+the time you need. Think about when exactly the callback function is executed, 
+and what that means for the variables involved in this example
+
+*/
+
+(function () {
+  const header = document.querySelector('h1');
+  header.style.color = 'red';
+
+  document.querySelector('body').addEventListener('click', function () {
+    header.style.color = 'blue';
+  });
+})();
 
 ///////////////////////////////////////
 // More Closure Examples
+/*
 
 // Example 1
 let f;
@@ -47,7 +70,7 @@ boardPassengers(180, 3);
 // setTimeout(function () {
 //   console.log('TIMER');
 // }, 1000);
-
+*/
 ///////////////////////////////////////
 // Closures
 // A closure makes a function remember all the variables that existed at the function's birthplace
