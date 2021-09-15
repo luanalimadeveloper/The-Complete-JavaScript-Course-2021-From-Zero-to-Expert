a92fbce79160e1e34e2128dc647eb511b12cbbe0
luanalimadeveloper <luanalima.developer@gmail.com>
2021-09-15 11:28:56 -0300

    The Filter Method

--- 11-Arrays-Bankist/script.js
+++ 11-Arrays-Bankist/script.js
@@ -84,6 +84,7 @@ displayMovements(account1.movements);
 // create a new array - map
 // modify (mutate) a array - for each
 
+/*
 const createUsernames = function (accs) {
   accs.forEach(function (acc) {
     acc.username = acc.owner
@@ -96,7 +97,7 @@ const createUsernames = function (accs) {
 
 createUsernames(accounts);
 console.log(accounts);
-
+*/
 /////////////////////////////////////////////////
 /////////////////////////////////////////////////
 // LECTURES
@@ -286,3 +287,22 @@ const movementsDescriptions = movements.map(
 );
 console.log(movementsDescriptions);
 */
+
+/////////////////////////////////////////////////
+// The Filter Method
+// Filter elements that satisfy some conditions
+
+const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+
+const deposits = movements.filter(function (mov, i, arr) {
+  return mov > 0;
+});
+console.log(movements);
+console.log(deposits);
+
+const depositsFor = [];
+for (const mov of movements) if (mov > 0) depositsFor.push(mov);
+console.log(depositsFor);
+
+const withdrawals = movements.filter(mov => mov < 0);
+console.log(withdrawals);
