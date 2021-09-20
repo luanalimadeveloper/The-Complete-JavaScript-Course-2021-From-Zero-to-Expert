e95da43d37fc0b8d4d61693b87d462f958ab318a
luanalimadeveloper <luanalima.developer@gmail.com>
2021-09-20 11:31:56 -0300

    Coding Challenges #3

--- 11-Arrays-Bankist/script.js
+++ 11-Arrays-Bankist/script.js
@@ -62,7 +62,7 @@ const inputTransferAmount = document.querySelector('.form__input--amount');
 const inputLoanAmount = document.querySelector('.form__input--loan-amount');
 const inputCloseUsername = document.querySelector('.form__input--user');
 const inputClosePin = document.querySelector('.form__input--pin');
-
+/*
 const displayMovements = function (movements) {
   containerMovements.innerHTML = '';
   // .textContent = 0;
@@ -126,7 +126,7 @@ const createUsernames = function (accs) {
 
 createUsernames(accounts);
 console.log(accounts);
-
+*/
 /////////////////////////////////////////////////
 /////////////////////////////////////////////////
 // LECTURES
@@ -439,6 +439,7 @@ console.log(calcAverageHumanAge(ageDogsArray2));
 /////////////////////////////////////////////////
 // The Magic of Chaining Methods
 
+/*
 const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 const eurToUsd = 1.1;
 console.log(movements);
@@ -454,3 +455,28 @@ const totalDepositUSD = movements
   .reduce((acc, mov) => acc + mov, 0);
 
 console.log(totalDepositUSD);
+*/
+
+/////////////////////////////////////////////////
+// Coding Challenges #3
+
+/*
+Rewrite the 'calcAverageHumanAge' function from Challenge #2, but this time 
+as an arrow function, and using chaining!
+
+Test data:
+- Data 1: [5, 2, 4, 1, 15, 8, 3]
+- Data 2: [16, 6, 10, 5, 6, 1, 4]
+*/
+
+const ageDogsArray1 = [5, 2, 4, 1, 15, 8, 3];
+const ageDogsArray2 = [16, 6, 10, 5, 6, 1, 4];
+
+const calcAverageHumanAge = ages =>
+  ages
+    .map(age => (age <= 2 ? 2 * age : 16 + age * 4))
+    .filter(age => age >= 18)
+    .reduce((acc, age, i, arr) => acc + age / arr.length, 0);
+
+console.log(calcAverageHumanAge(ageDogsArray1));
+console.log(calcAverageHumanAge(ageDogsArray2));
