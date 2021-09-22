ec210d623de5d625f7bbf967d749eb0adcc75324
luanalimadeveloper <luanalima.developer@gmail.com>
2021-09-22 16:26:53 -0300

    Sorting Arrays

--- 11-Arrays-Bankist/script.js
+++ 11-Arrays-Bankist/script.js
@@ -63,11 +63,14 @@ const inputLoanAmount = document.querySelector('.form__input--loan-amount');
 const inputCloseUsername = document.querySelector('.form__input--user');
 const inputClosePin = document.querySelector('.form__input--pin');
 
-const displayMovements = function (movements) {
+const displayMovements = function (movements, sort = false) {
   containerMovements.innerHTML = '';
   // .textContent = 0;
 
-  movements.forEach(function (mov, i) {
+  // slice - make a copy to not change the original
+  const movs = sort ? movements.slice().sort((a, b) => a - b) : movements;
+
+  movs.forEach(function (mov, i) {
     const type = mov > 0 ? 'deposit' : 'withdrawal';
 
     const html = `
@@ -231,6 +234,12 @@ btnClose.addEventListener('click', function (e) {
   inputCloseUsername.value = inputClosePin.value = '';
 });
 
+let sorted = false;
+btnSort.addEventListener('click', function (e) {
+  e.preventDefault();
+  displayMovements(currentAccount.movements, !sorted);
+  sorted = !sorted;
+});
 /////////////////////////////////////////////////
 /////////////////////////////////////////////////
 // LECTURES
@@ -636,6 +645,7 @@ console.log(movements.filter(deposit));
 /////////////////////////////////////////////////
 // Flat and flatmap
 
+/*
 const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 
 const arr = [[1, 2, 3], [4, 5, 6], 7, 8];
@@ -663,3 +673,43 @@ const overalBalance3 = accounts
   .flatMap(acc => acc.movements)
   .reduce((acc, mov) => acc + mov, 0);
 console.log(overalBalance3);
+*/
+
+/////////////////////////////////////////////////
+// Sortig Arrays
+
+const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+
+const owners = ['Jonas', 'Zach', 'Adam', 'Martha'];
+// from A to Z
+console.log(owners.sort());
+console.log(owners);
+
+// Numbers
+console.log(movements);
+
+// not sort because sort method is for string
+//console.log(movements.sort());
+
+// return < 0, A, B (keep order)
+// return > 0, B, A (switch order)
+
+// Ascendind
+movements.sort((a, b) => {
+  if (a > b) return 1;
+  if (a < b) return -1;
+});
+console.log(movements);
+
+movements.sort((a, b) => a - b);
+console.log(movements);
+
+// Descending
+movements.sort((a, b) => {
+  if (a > b) return -1;
+  if (a < b) return 1;
+});
+console.log(movements);
+
+movements.sort((a, b) => b - a);
+console.log(movements);
