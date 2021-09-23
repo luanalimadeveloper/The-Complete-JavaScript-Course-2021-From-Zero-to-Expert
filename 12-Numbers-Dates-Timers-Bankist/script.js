01ea9e12234d05915a5328befa00f78a9f81c591
luanalimadeveloper <luanalima.developer@gmail.com>
2021-09-23 20:17:24 -0300

    The Remainder Operator

--- 12-Numbers-Dates-Timers-Bankist/script.js
+++ 12-Numbers-Dates-Timers-Bankist/script.js
@@ -295,7 +295,7 @@ console.log(Number.isInteger(23 / 0));
 
 /////////////////////////////////////////////////
 // Math and Rouding
-
+/*
 console.log(Math.sqrt(25));
 console.log(25 ** (1 / 2));
 console.log(25 ** (1 / 3));
@@ -339,3 +339,29 @@ console.log(Math.floor(-23.3));
 console.log((2.7).toFixed(0));
 console.log((2.7).toFixed(3));
 console.log(+(2.345).toFixed(2));
+*/
+
+/////////////////////////////////////////////////
+// The Remainder Operator
+
+console.log(5 % 2);
+console.log(5 / 2); // 5 = 2 * 2 + 1
+
+console.log(8 % 3);
+console.log(8 / 3); // 8 = 2 * 3 + 2
+
+console.log(6 % 2);
+console.log(6 / 2);
+
+const isEven = n => n % 2 === 0;
+console.log(isEven(8));
+console.log(isEven(23)); // odd number
+
+labelBalance.addEventListener('click', function () {
+  [...document.querySelectorAll('.movements__row')].forEach(function (row, i) {
+    // 0, 2, 4
+    if (i % 2 === 0) row.style.backgroundColor = 'orangered';
+    // 3, 6, 9
+    if (i % 3 === 0) row.style.backgroundColor = 'blue';
+  });
+});
