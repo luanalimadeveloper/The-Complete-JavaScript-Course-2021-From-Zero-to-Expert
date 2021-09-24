a0db879ea9c570ebb788d19d4b33eaa1ab51ce47
luanalimadeveloper <luanalima.developer@gmail.com>
2021-09-23 22:59:49 -0300

    Creating Dates

--- 12-Numbers-Dates-Timers-Bankist/script.js
+++ 12-Numbers-Dates-Timers-Bankist/script.js
@@ -368,7 +368,7 @@ labelBalance.addEventListener('click', function () {
 */
 /////////////////////////////////////////////////
 // Working with BigInt
-
+/*
 // safe
 console.log(2 ** 53 - 1);
 console.log(Number.MAX_SAFE_INTEGER);
@@ -402,3 +402,42 @@ console.log(huge + ' is a really big !!!!');
 // Divisions
 console.log(14n / 3n);
 console.log(10 / 3);
+*/
+
+/////////////////////////////////////////////////
+// Creating Dates
+
+// Create a date
+
+const now = new Date();
+console.log(now);
+
+console.log(new Date('Sep 23 2021 22:27:23'));
+console.log(new Date('December 24, 2020'));
+console.log(new Date(account1.movementsDates[0]));
+
+console.log(new Date(2037, 10, 19, 15, 23, 5));
+console.log(new Date(2037, 10, 31));
+
+console.log(new Date(0));
+console.log(new Date(3 * 24 * 60 * 60 * 1000));
+
+// Working with dates
+const future = new Date(2037, 10, 19, 15, 23, 5);
+console.log(future);
+console.log(future.getFullYear());
+console.log(future.getMonth());
+console.log(future.getDate());
+console.log(future.getDay()); // Thu = 4
+console.log(future.getHours());
+console.log(future.getMinutes());
+console.log(future.getSeconds());
+console.log(future.toISOString());
+console.log(future.getTime());
+
+console.log(new Date(2142267785000));
+
+console.log(Date.now());
+
+future.setFullYear(2040);
+console.log(future);
