194e0a5777d6701925bb71fd497a604e3a062900
luanalimadeveloper <luanalima.developer@gmail.com>
2021-09-28 13:34:39 -0300

    Styles, Attributes and Classes

--- 13-Advanced-DOM-Bankist/script.js
+++ 13-Advanced-DOM-Bankist/script.js
@@ -80,3 +80,58 @@ document
     message.remove();
     // message.parentElement.removeChild(message);
   });
+
+//////////////////////////////////////
+// Styles, Attributes and Classes
+
+// Styles
+message.style.backgroundColor = '#37383d';
+message.style.width = '120%';
+
+console.log(message.style.height);
+
+console.log(message.style.color);
+console.log(message.style.backgroundColor);
+
+console.log(getComputedStyle(message).color);
+console.log(getComputedStyle(message).height);
+
+message.style.height =
+  Number.parseFloat(getComputedStyle(message).height, 10) + 30 + 'px';
+
+// name of teh property and value
+document.documentElement.style.setProperty('--color-primary', 'orangered');
+
+// Attributes
+
+const logo = document.querySelector('.nav__logo');
+console.log(logo.alt);
+console.log(logo.src);
+console.log(logo.className);
+
+logo.alt = 'Beautiful minimalist logo';
+
+// not work => not a standart propert expected to be on images
+console.log(logo.designer);
+console.log(logo.getAttribute('designer'));
+
+logo.setAttribute('company', 'Bankist'); // new attribute
+
+console.log(logo.src);
+console.log(logo.getAttribute('src'));
+
+const link = document.querySelector('.twitter-link');
+console.log(link.href);
+console.log(link.getAttribute('href'));
+
+// Data attributes
+console.log(logo.dataset.versionNumber);
+
+// Classes
+logo.classList.add('c', 'j');
+logo.classList.remove('c');
+logo.classList.toggle('c');
+logo.classList.contains('c'); // not includes
+
+// Don't use
+logo.className = 'jonas';
