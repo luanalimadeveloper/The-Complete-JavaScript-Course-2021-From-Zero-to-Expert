62f20148aec5d7b77de717ba50ec3630c8e0a2eb
luanalimadeveloper <luanalima.developer@gmail.com>
2021-09-28 15:59:56 -0300

    Implementing Smooth Scrolling

--- 13-Advanced-DOM-Bankist/script.js
+++ 13-Advanced-DOM-Bankist/script.js
@@ -36,7 +36,7 @@ document.addEventListener('keydown', function (e) {
 
 ///////////////////////////////////////
 //Selecting, Creating, and Deleting Elements
-
+/*
 // Selecting elements
 
 console.log(document.documentElement);
@@ -135,3 +135,44 @@ logo.classList.contains('c'); // not includes
 
 // Don't use
 logo.className = 'jonas';
+*/
+
+//////////////////////////////////////
+//Implementing Smooth Scrolling
+
+const btnScrollTo = document.querySelector('.btn--scroll-to');
+const section1 = document.querySelector('#section--1');
+
+btnScrollTo.addEventListener('click', function (e) {
+  const s1coords = section1.getBoundingClientRect();
+  console.log(s1coords);
+
+  console.log(e.target.getBoundingClientRect());
+
+  console.log('Current scroll (X/Y)', window.pageXOffset, window.pageYOffset);
+
+  console.log(
+    'height/width viewport',
+    document.documentElement.clientHeight,
+    document.documentElement.clientWidth
+  );
+
+  // Old way to Scroll the page
+
+  // Scrolling
+  // The current position + the current scroll
+
+  // window.scrollTo(
+  //   s1coords.left + window.pageXOffset,
+  //   s1coords.top + window.pageYOffset
+  // );
+
+  // window.scrollTo({
+  //   left: s1coords.left + window.pageXOffset,
+  //   top: s1coords.top + window.pageYOffset,
+  //   behavior: 'smooth',
+  // });
+
+  // New way Scroll the page
+  section1.scrollIntoView({ behavior: 'smooth' });
+});
