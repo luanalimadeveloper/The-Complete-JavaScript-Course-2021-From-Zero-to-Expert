72d4077baf315ca5c9561e97e92e6775e74d4522
luanalimadeveloper <luanalima.developer@gmail.com>
2021-10-04 22:21:17 -0300

    Setters and Getters

--- 14-OOP/script.js
+++ 14-OOP/script.js
@@ -133,6 +133,7 @@ car2.brake();
 
 ///////////////////////////////////////
 // ES6 Classes
+
 //Classes in JavaScript are synthetic sugar - implement prototypal inheritance behind the scenes
 
 // Behind the scenes classes are still functions
@@ -141,8 +142,8 @@ car2.brake();
 
 // class declaration
 class PersonCl {
-  constructor(firstName, birthYear) {
-    this.firstName = firstName;
+  constructor(fullName, birthYear) {
+    this.fullName = fullName;
     this.birthYear = birthYear;
   }
 
@@ -152,13 +153,29 @@ class PersonCl {
   }
 
   greet() {
-    console.log(`Hey ${this.firstName}`);
+    console.log(`Hey ${this.fullName}`);
+  }
+
+  get age() {
+    return 2037 - this.birthYear;
+  }
+
+  // Set a property that already exists
+  set fullName(name) {
+    console.log(name);
+    if (name.includes(' ')) this._fullName = name;
+    else alert(`${name} is not a full name!`);
+  }
+
+  get fullName() {
+    return this._fullName;
   }
 }
 
-const jessica = new PersonCl('Jessica', 1996);
+const jessica = new PersonCl('Jessica Davis', 1996);
 console.log(jessica);
 jessica.calcAge();
+console.log(jessica.age);
 
 console.log(jessica.__proto__ === PersonCl.prototype);
 
@@ -171,3 +188,26 @@ jessica.greet();
 // 1. Classes are NOT hoisted : We cant use them before they are declared in the code.
 // 2. Classes are first-class citizes : pass them into functions and also return them from functions.
 // 3. Classes are executed in strict mode.
+
+const walter = new PersonCl('Walter White', 1965);
+
+///////////////////////////////////////
+// Setters and Getters
+
+const account = {
+  owner: 'Jonas',
+  movements: [200, 530, 120, 300],
+
+  get latest() {
+    return this.movements.slice(-1).pop();
+  },
+
+  set latest(mov) {
+    this.movements.push(mov);
+  },
+};
+
+console.log(account.latest);
+
+account.latest = 50;
+console.log(account.movements);
