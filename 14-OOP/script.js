226207e4ed36774586bbfdad9ffc84dcff66d5d5
luanalimadeveloper <luanalima.developer@gmail.com>
2021-10-06 19:58:42 -0300

    Encapsulation: Private Class Fields and Methods

--- 14-OOP/script.js
+++ 14-OOP/script.js
@@ -505,46 +505,85 @@ jay.calcAge();
 ///////////////////////////////////////
 // Another Class Example
 
+// 1) Public fields
+// 2) Private fields
+// 3) Public methods
+// 4) Private methods
+// (there is also the static version)
+
 class Account {
+  // 1) Public fields (instances)
+  locale = navigator.languages;
+
+  // 2) Private fields
+  #movements = [];
+  #pin;
+
   constructor(owner, currency, pin) {
     this.owner = owner;
     this.currency = currency;
-    this.pin = pin;
-    this.movements = [];
-    this.locale = navigator.language;
+    this.#pin = pin;
+    // Protected property
+    //this._movements = [];
+    //this.locale = navigator.language;
 
     console.log(`Thanks for opening an account, ${owner}`);
   }
+
+  // 3) public methods
+  // Public interface
+  getMovements() {
+    return this.#movements;
+  }
+
   // Public interface
   deposit(val) {
-    this.movements.push(val);
+    this.#movements.push(val);
   }
 
   withdraw(val) {
     this.deposit(-val);
   }
 
-  approveLoan(val) {
+  _approveLoan(val) {
     return true;
   }
 
   requestLoan(val) {
-    if (this.approveLoan(val)) {
+    if (this._approveLoan(val)) {
       this.deposit(val);
       console.log(`Loan approved`);
     }
+  }
+
+  static helper() {
+    console.log('Helper');
+  }
+
+  // 4) Private methods
+  _approveLoan(val) {
+    return true;
   }
 }
 
 const acc1 = new Account('Jonas', 'EUR', 1111);
 console.log(acc1);
 
-// acc1.movements.push(250);
-// acc1.movements.push(-140);
+// acc1._movements.push(250);
+// acc1._movements.push(-140);
 acc1.deposit(250);
 acc1.withdraw(140);
 acc1.requestLoan(1000);
-acc1.approveLoan(1000);
+acc1._approveLoan(1000);
+
+console.log(acc1.getMovements());
 
 console.log(acc1);
 console.log(acc1.pin);
+
+//console.log(acc1.#movements);
+//console.log(acc1.#pin);
+
+//console.log(acc1.#approveLoan(100));
+
+Account.helper();
