3fd87c8ec4511d494ff654d86de7c2c1393f6ae8
luanalimadeveloper <luanalima.developer@gmail.com>
2021-10-18 14:21:44 -0300

    The Module Pattern

--- 17-Modern-JS-Modules-Tooling/script.js
+++ 17-Modern-JS-Modules-Tooling/script.js
@@ -1,4 +1,6 @@
 // Importing module
+
+/*
 //import { addToCart, totalPrice as price, tq } from './shoppingCart.js';
 
 // addToCart('bread', 5);
@@ -17,3 +19,38 @@ add('bread', 5);
 add('apples', 4);
 
 console.log(cart);
+*/
+
+///////////////////////////////////////////////////
+// The Module Pattern
+
+// create a new scope and return data just once.
+const ShoppingCart2 = (function () {
+  const cart = [];
+  const shippingCost = 10;
+  const totalPrice = 237;
+  const totalQuantity = 23;
+
+  const addToCart = function (product, quantity) {
+    cart.push({ product, quantity });
+    console.log(
+      `${quantity} ${product} added to cart (shipping cost is ${shippingCost})`
+    );
+  };
+
+  const orderStock = function (product, quantity) {
+    console.log(`${quantity} ${product} ordered from supplier`);
+  };
+
+  return {
+    addToCart,
+    cart,
+    totalPrice,
+    totalQuantity,
+  };
+})();
+
+ShoppingCart2.addToCart('apple', 4);
+ShoppingCart2.addToCart('pizza', 2);
+console.log(ShoppingCart2);
+console.log(ShoppingCart2.shippingCost);
